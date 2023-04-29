fd017754c16392fc3aa8738a7ee28837c874cf30
James Coburn <61681076+JamesCoburn246@users.noreply.github.com>
2023-04-30 02:16:27 +1200

    Fledge out more analytics features

--- Assets/Scripts/Analytics/UnityAnalytics.cs
+++ Assets/Scripts/Analytics/UnityAnalytics.cs
@@ -1,3 +1,4 @@
+using System.Collections.Generic;
 using Unity.Services.Analytics;
 using Unity.Services.Core;
 using UnityEngine;
@@ -6,16 +7,57 @@ namespace Analytics
 {
     public class UnityAnalytics : MonoBehaviour
     {
+        private static bool optedOut = false;
+
+        // TODO https://docs.unity.com/analytics/en/manual/EventsTracking
+
         private async void Start()
         {
             try
             {
                 await UnityServices.InitializeAsync();
+                List<string> consentIdentifiers = await AnalyticsService.Instance.CheckForRequiredConsents();
+                if (consentIdentifiers.Count == 0)
+                {
+                    Debug.Log("The user has provided all required consents for analytic tracking.");
+                }
+                else
+                {
+                    Debug.Log("The user has yet to provide all required consents for analytic tracking.");
+                    DisplayPrivacyInformation();
+                }
             }
             catch (ConsentCheckException e)
             {
-                Debug.Log(e.ToString());
+                // Something went wrong when checking the GeoIP, check the e.Reason and handle appropriately.
+                Debug.Log(e.Reason);
             }
+        }
+
+        public void OptOut()
+        {
+            try
+            {
+                if (!optedOut)
+                {
+                    // Show a GDPR/COPPA/other opt-out consent flow
+                    // If a user opts out
+                    AnalyticsService.Instance.OptOut();
+                }
+                // Record that we have checked a user's consent, so we don't repeat the flow unnecessarily.
+                // In a real game, use PlayerPrefs or an equivalent to persist this state between sessions
+                optedOut = true;
+            }
+            catch (ConsentCheckException e)
+            {
+                // Handle the exception by checking e.Reason
+                Debug.Log(e.Reason);
+            }
+        }
+
+        public static void DisplayPrivacyInformation()
+        {
+            Application.OpenURL(AnalyticsService.Instance.PrivacyUrl);
         }
     }
 }
