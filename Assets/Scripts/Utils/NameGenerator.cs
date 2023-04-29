aaac8422bde840ad3959e74a33169da65851835b
James Coburn <61681076+JamesCoburn246@users.noreply.github.com>
2023-04-29 19:27:01 +1200

    Add separator sequence

--- Assets/Scripts/Utils/NameGenerator.cs
+++ Assets/Scripts/Utils/NameGenerator.cs
@@ -12,12 +12,16 @@ public class NameGenerator : MonoBehaviour
     [SerializeField]
     [Tooltip("The comma-separated list of nouns to generate names from.")]
     private TextAsset listOfNouns;
+    [SerializeField]
+    [Tooltip("The character sequence to add between the adjective and the noun.")]
+    private string separator = "";
 
     public string GenerateName()
     {
         // Get random name
         StringBuilder generatedName = new();
         generatedName.Append(GetRandomAdjectiveFromFile());
+        generatedName.Append(separator);
         generatedName.Append(GetRandomNounFromFile());
 
         return generatedName.ToString();
