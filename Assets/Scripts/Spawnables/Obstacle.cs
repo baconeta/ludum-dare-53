063328c5a79b60bbf4aae8bebfc09b809e458e2f
Tessa Power <tessa@tessapower.co>
2023-04-29 20:38:42 +1200

    Add Poolable object to Rock prefab

--- Assets/Scripts/Spawnables/Obstacle.cs
+++ Assets/Scripts/Spawnables/Obstacle.cs
@@ -1,4 +1,4 @@
-using System;
+using ObjectPooling;
 using UnityEngine;
 
 namespace Spawnables
@@ -7,21 +7,45 @@ namespace Spawnables
     {
         // TODO: decide on appropriate values for these fields
         [SerializeField] private float speed = 0.1f;
-        [SerializeField] public float damage = 5.0f;
+        [SerializeField] private float damage = 5.0f;
 
-        private void Update()
-        {
+        private float _bottomBound;
+        private Poolable _poolable;
+
+        private void Start() {
+            _bottomBound = GameObject.Find("BottomBound").transform.position.y;
+            _poolable = GetComponent<Poolable>();
+        }
+
+        private void Update() {
             // Slowly move the obstacle down the screen
             transform.position += Vector3.down * (speed * Time.deltaTime);
+            // if the obstacle goes off screen, recycle it
+            if (transform.position.y < _bottomBound)
+            {
+                _poolable.Recycle();
+            }
         }
         
         private void OnCollisionEnter(Collision collision)
         {
-            if (collision.gameObject.CompareTag("Player"))
+            if (collision.gameObject.CompareTag("Ferry"))
             {
                 // TODO: determine game state and damage player appropriately (is this for the player to do?)
                 Debug.Log("Player collided with obstacle");
             }
         }
+
+        public void MultiplySpeed(float multiplier)
+        {
+            speed *= multiplier;
+        }
+
+        public void MultiplyDamage(float multiplier)
+        {
+            damage *= multiplier;
+        }
+
+        public float Damage => damage;
     }
 }
