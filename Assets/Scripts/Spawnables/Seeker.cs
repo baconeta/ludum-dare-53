f36fcd66ed1897fef6453c6bb95aafec6dc631d1
Tessa Power <tessa@tessapower.co>
2023-05-01 08:51:29 +1200

    Move seeker in a "pulling" fashion

--- Assets/Scripts/Spawnables/Seeker.cs
+++ Assets/Scripts/Spawnables/Seeker.cs
@@ -7,6 +7,12 @@ namespace Spawnables
         private GameObject _target;
         private const float RotationSpeed = 5.0f;
 
+        [Tooltip("The amount of time in seconds to spend seeking the target.")] [SerializeField]
+        private float seekDuration = 1.0f;
+        
+        [Tooltip("The amount of time in seconds to spend idly going with the river flow")] [SerializeField]
+        private float idleDuration = 1.0f;
+
         private void OnEnable() {
             // Get a lock on the player so we can follow them
             _target = GameObject.FindWithTag("Ferry");
@@ -15,27 +21,46 @@ namespace Spawnables
             
             if (!GameStateManager.Instance.IsGameActive()) return;
             
-            // TODO: only change direction every so often
-            
             // If the target is null, find it
             _target ??= GameObject.FindWithTag("Ferry");
-            
+
             // Rotate to face the target
             var dir = (_target.transform.position - transform.position).normalized;
+            RotateTowardTarget(dir);
+            Move(dir);
+        }
+        
+        /**
+         * Rotate to look toward the target.
+         */
+        private void RotateTowardTarget(Vector3 dir)
+        {
             // Look in the direction of the target
             var rot = Quaternion.LookRotation(dir, Vector3.back);
             // Smoothly rotate towards the target
             rot = Quaternion.Lerp(transform.rotation, rot, RotationSpeed * Time.deltaTime);
             // Set the rotation on the z axis only (because 2D)
             transform.eulerAngles = new Vector3(0, 0, rot.eulerAngles.z);
+        }
 
-            // TODO: only do this every so often
-            // Take a step in the direction of the target
+        /**
+         * Move toward the target in a stepwise fashion.
+         */
+        private void Move(Vector3 dir) {
+            // Figure out amount to move this frame
             var step = currentSpeed * Time.deltaTime;
-
-            dir += Vector3.down;
-            // Slowly move the obstacle down the screen
-            transform.position += dir * step;
+            if (Time.time % (idleDuration + seekDuration) < idleDuration)
+            {
+                // Slowly move the obstacle down the screen with the river flow
+                transform.position += Vector3.down * step;
+            }
+            else
+            {
+                // Move toward the target
+                dir += Vector3.down;
+                transform.position += dir * step;
+            }
         }
     }
+    
 }
