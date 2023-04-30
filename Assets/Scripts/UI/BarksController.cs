c2f386d6e8710eacb048956c5bd0e52690f0f642
Josh Pearson <36744690+baconeta@users.noreply.github.com>
2023-04-30 23:42:54 +0300

    working popup system

--- Assets/Scripts/UI/BarksController.cs
+++ Assets/Scripts/UI/BarksController.cs
@@ -1,5 +1,7 @@
+using System;
 using System.Collections;
 using UnityEngine;
+using UnityEngine.UIElements;
 
 public class BarksController : MonoBehaviour
 {
@@ -20,13 +22,18 @@ public class BarksController : MonoBehaviour
         BoatController.OnDamageTaken -= HitSomething;
     }
 
-    private void ShowLeftSpeaker(string speaker, string bark)
+    private void Start()
+    {
+        _timeLastBarkClosed = Time.time;
+    }
+
+    private void SetLeftSpeaker(string speaker, string bark)
     {
         barkPopupLeft.SetSpeaker(speaker);
         barkPopupLeft.SetText(bark);
     }
 
-    private void ShowRightSpeaker(string speaker, string bark)
+    private void SetRightSpeaker(string speaker, string bark)
     {
         barkPopupRight.SetSpeaker(speaker);
         barkPopupRight.SetText(bark);
@@ -36,25 +43,34 @@ public class BarksController : MonoBehaviour
     {
     }
 
-    private void HitSomething()
+    public void HitSomething()
     {
         TryBark(true);
     }
 
     private void TryBark(bool damageBark)
     {
-        if (_isBarkOnScreen || minTimeBetweenBarks < Time.time - _timeLastBarkClosed)
+        float secondsSinceLastBark = Time.time - _timeLastBarkClosed;
+        if (_isBarkOnScreen || secondsSinceLastBark < minTimeBetweenBarks)
         {
+            Debug.Log("Can't bark yet");
             return;
         }
 
+        _isBarkOnScreen = true;
+
         if (damageBark)
         {
+            Debug.Log("Damage bark");
             // Get a new bark and speaker from the soul factory
             // Get a return quip for Charon 
 
             // calculate the bark time showing length
             // Set up bark popups and show it on screen
+            SetLeftSpeaker("Testing", "How dare you.");
+            SetRightSpeaker("James", "Okay bro");
+            StartCoroutine(ShowPopup(barkPopupLeft, 3));
+            StartCoroutine(ShowPopup(barkPopupRight, 3, 1));
         }
         else
         {
@@ -63,15 +79,18 @@ public class BarksController : MonoBehaviour
         }
     }
 
-    private IEnumerator ShowPopup(BarkPopup popup, int timeOnScreen)
+    private IEnumerator ShowPopup(BarkPopup popup, float timeOnScreen, float delay = 0f)
     {
+        yield return new WaitForSeconds(delay);
         // Fade popup in
-        // Show for timeOnScreenSeconds
+        popup.gameObject.SetActive(true);
 
+        // Show for timeOnScreenSeconds
         yield return new WaitForSeconds(timeOnScreen);
 
         // Fade Out
-
+        popup.gameObject.SetActive(false);
         _timeLastBarkClosed = Time.time;
+        _isBarkOnScreen = false;
     }
 }
