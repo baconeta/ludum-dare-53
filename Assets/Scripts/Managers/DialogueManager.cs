d4fa29d5fe2979d67934607fbb5bdd6fd1c698b1
Brayden Cooke <Braydencooke@hotmail.com>
2023-04-30 13:01:28 +1200

    Added Dialogue
    - Dialogue now appears when entering Ferrying or Returning states.
    - TAB to cycle dialogue.
    - Cannot launch voyage while in dialogue.

--- Assets/Scripts/Managers/DialogueManager.cs
+++ Assets/Scripts/Managers/DialogueManager.cs
@@ -1,31 +1,96 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[Serializable]
 public struct DialogueStruct
 {
     public List<string> linesOfDialogue;
 }
 public class DialogueManager : MonoBehaviour
 {
+    public static DialogueManager instance;
+    
+    //TODO Expand dialogues to include event specific dialogues: e.g. Returning/Ferrying/Start/End
     public List<DialogueStruct> Dialogues;
     public DialogueStruct currentDialogue;
-    public int currentDialogue;
+    public int currentDialogueIndex;
+    public int currentDialogueLine;
+    public bool isDialogueActive;
 
-    public int currentLine = 0;
-    
     public KeyCode nextLine;
 
-    void StartDialogue()
+    public static event Action OnDialogueStart; 
+    public static event Action OnDialogueEnd; 
+
+    private void Awake()
     {
-        currentLine = 0;
+        if (!instance) instance = this;
+        else Destroy(this);
+    }
+    
+    private void OnEnable()
+    {
+        GameStateManager.OnFerryingEnter += StartDialogue;
+        GameStateManager.OnReturningEnter += StartDialogue;
     }
 
-    void Update()
+    private void OnDisable()
     {
-        if (Input.GetKeyDown(nextLine))
+        GameStateManager.OnFerryingEnter -= StartDialogue;
+        GameStateManager.OnReturningEnter -= StartDialogue;
+    }
+
+    public void StartDialogue()
+    {
+        //Get the current dialogue
+        currentDialogue = Dialogues[currentDialogueIndex];
+        currentDialogueLine = 0;
+        
+        
+        //Show UI/Update Text
+        OnDialogueStart?.Invoke();
+        isDialogueActive = true;
+    }
+
+    public string GetCurrentLine()
+    {
+        return (currentDialogue.linesOfDialogue[currentDialogueLine]);
+    }
+    public string NextLine()
+    {
+        //Increment currentDialogueLine
+        currentDialogueLine++;
+        //Has it passed the last line?
+        if (currentDialogueLine >= currentDialogue.linesOfDialogue.Count)
         {
-            
+            //Yes, dialogue exhausted. End Dialogue.
+            EndDialogue();
+            return "";
         }
+        //return the current line
+        return GetCurrentLine();
+    }
+
+    public void EndDialogue()
+    {
+        currentDialogueIndex++;
+        //Has we exhausted all lines of dialogue?
+        if (currentDialogueIndex >= Dialogues.Count)
+        {
+            //TODO Create logic for reusing dialogue.
+            //TEMP** Reset to first dialogue.
+            currentDialogueIndex = 0;
+        }
+        //Hide UI
+        OnDialogueEnd?.Invoke();
+        isDialogueActive = false;
+
+    }
+
+    public bool IsDialogueActive()
+    {
+        return isDialogueActive;
     }
 }
