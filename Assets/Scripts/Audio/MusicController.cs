611beba44929ac0f0e46b7a60a184696c99ccfc4
Tomas Morton <tomas.morton@hotmail.co.nz>
2023-04-29 21:54:25 -0400

    Use wrapper to play

--- Assets/Scripts/Audio/MusicController.cs
+++ Assets/Scripts/Audio/MusicController.cs
@@ -1,19 +1,14 @@
+using Audio;
 using Managers;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Audio;
 
 public class MusicController : MonoBehaviour
 {
-    // Start is called before the first frame update
     void Start()
     {
-        AudioManager.Instance.Play("game-background-music");
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        AudioWrapper.Instance.PlaySound("game-background-music");
     }
 }
