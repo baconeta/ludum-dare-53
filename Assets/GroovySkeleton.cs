3bad3f8f4cecf48c6981e0b84add3e225bc938c8
Brayden Cooke <87751532+Cookiezznz@users.noreply.github.com>
2023-05-02 13:24:31 +1200

    SkeleStones are a go

---
+++ Assets/GroovySkeleton.cs
@@ -0,0 +1,49 @@
+using Managers;
+using Spawnables;
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class GroovySkeleton : MonoBehaviour
+{
+    private Transform _target;
+    private bool hasThrown;
+    public float throwDistance;
+    public GameObject throwPrefab;
+    public Transform throwStartPoint;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _target = GameObject.FindWithTag("Ferry").transform;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(GameStateManager.Instance.IsGameActive())
+        {
+            if(Vector3.Distance(transform.position, _target.position) < throwDistance)
+            {
+                if(!hasThrown)
+                    ///TODO Replace with animation event
+                    Throw();
+            }
+        }
+    }
+
+    void Throw()
+    {
+        hasThrown = true;
+        //Need to instantiate on animation event.
+        Yeet newYeet = Instantiate(throwPrefab, transform).GetComponent<Yeet>();
+        newYeet.transform.position = throwStartPoint.position;
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.yellow;
+        Gizmos.DrawWireSphere(transform.position, throwDistance);
+        }
+}
--- Assets/Scripts/Spawnables/Yeet.cs
+++ Assets/Scripts/Spawnables/Yeet.cs
@@ -1,13 +1,20 @@
+using System.Collections;
 using UnityEngine;
 
 namespace Spawnables {
     public class Yeet : MonoBehaviour {
         private GameObject _target;
+        private Vector3 _direction;
         private Vector3 _end;
         private bool _triggered;
+        public float disabledDuration;
+        private CircleCollider2D _circleCollider2D;
 
         private void Start() {
+            _circleCollider2D = GetComponent<CircleCollider2D>();
             _target = GameObject.FindWithTag("Ferry");
+            _direction = _target.transform.position - transform.position;
+            _end = _target.transform.position;
         }
 
         // Update is called once per frame
@@ -15,13 +22,22 @@ namespace Spawnables {
             if (!_triggered) return;
             
             // Move the object towards the target
-            transform.position = Vector3.MoveTowards(transform.position, _end, 0.1f);
+            transform.position = Vector3.MoveTowards(_direction, _end, 0.1f);
+        }
+
+        private IEnumerator enableCollision()
+        {
+            yield return new WaitForSeconds(disabledDuration);
+            _circleCollider2D.enabled = true;
+            yield return null;
         }
 
         public void Yeeteth() {
             _triggered = true;
+            StartCoroutine(enableCollision());
             // Calculate end once based on where the target is
-            _end = _target.transform.position;
         }
+
+        
     }
 }
